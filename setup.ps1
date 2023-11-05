e1ee4513a9194be9e188cc7346fd5972138d0090
Yogesh Saini <111651161+YogeshxSaini@users.noreply.github.com>
2023-11-06 00:01:01 +0530

    Improved

--- setup.ps1
+++ setup.ps1
@@ -64,7 +64,7 @@ function Install-Driver {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     Write-Host "Chocolatey installed successfully." -ForegroundColor Green
     Write-Host "Installing Gecko Driver..." -ForegroundColor Yellow
-    choco install selenium-gecko-driver
+    choco install selenium-gecko-driver -y
     Write-Host "Gecko Driver installed successfully." -ForegroundColor Green
 }
 
