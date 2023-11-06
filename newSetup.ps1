ad0ab46680e6bd77fc53cceb7426246e0ae56d2e
Yogesh Saini <111651161+YogeshxSaini@users.noreply.github.com>
2023-11-06 12:51:16 +0530

    Create newSetup.ps1

---
+++ newSetup.ps1
@@ -0,0 +1,43 @@
+# Step 1
+# Go to User Directory
+cd ~
+
+# Change Execution Policy Settings
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+
+# Check List of Execution Policy
+Get-ExecutionPolicy -List
+
+# Step 2
+# Connect to WebClient (shares Internet connection settings)
+$script = New-Object Net.WebClient
+
+# Check if script is working + options (commands)
+$script | Get-Member
+
+# Download & Check Signatures
+$script.DownloadString("https://chocolatey.org/install.ps1")
+
+# Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Check if Chocolatey is installed
+choco -?
+
+# Update Chocolatey
+choco upgrade chocolatey
+
+# Step 3
+# Install Python via Chocolatey
+choco install -y python3
+
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+
+# Refresh Environment Vars
+refreshenv
+
+# Check if Python is installed
+python -V
+
+# Make Sure PIP is installed and upgraded
+python -m pip install --upgrade pip
